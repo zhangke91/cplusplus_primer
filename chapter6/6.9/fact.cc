4d09bab8b9485248e5190666dc98889408be9ba1
zhangke91 <zhangkegrand@gmail.com>
2019-04-19 21:55:49 +0000

    Add 6.9

---
+++ chapter6/6.9/fact.cc
@@ -0,0 +1,10 @@
+#include "chapter6.h"
+
+int fact(int val) {
+  int ret = 1;
+  while (val > 1) {
+    ret *= val--;
+  }
+  return ret;
+}
+
---
+++ chapter6/6.9/factmain.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "chapter6.h"
+
+using std::cout;
+using std::endl;
+int main() {
+  int k = 5;
+  cout << fact(k) << endl;
+  return 0;
+}
